13ad7d71ec8867804888e7919e4c6b0297c6c58e
unknown <boatenge4412@gmail.com>
2024-05-21 17:07:29 +0000

    e-commerce-update4.0

--- src/components/LoginPopup/LoginPopup.jsx
+++ src/components/LoginPopup/LoginPopup.jsx
@@ -1,12 +1,28 @@
-import React from 'react'
-import './LoginPopup.css'
+import React, { useState } from "react";
+import "./LoginPopup.css";
+import { assets } from "../../assets/assets";
 
-const LoginPopup = () => {
+const LoginPopup = (setShowLogin) => {
+
+  const [currState,setCurrState] = useState("Login")
   return (
-    <div className='login-popup'>
-      login
+    <div className="login-popup">
+      <form className="login-popup-container">
+        <div className="login-popup-title">
+          <h2>
+            {currState}
+          </h2>
+          <img onClick={()=>setShowLogin(false)} src={assets.cross_icon} alt="" />
+        </div>
+        <div className="login-popup-inputs">
+          <input type="text" placeholder="Your name" required />
+          <input type="email" placeholder="Your email" required />
+          <input type="password" placeholder="password" required />
+        </div>
+        <button>{currState==="Sign Up"? "Create account":"Login"}</button>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default LoginPopup
+export default LoginPopup;
